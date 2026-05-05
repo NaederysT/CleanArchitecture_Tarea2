1534e16fbfcaca8814ecb1ccc37adfc6f1551096
Benjamín Ayala <bayala@utem.cl>
2026-05-04 22:10:01 -0400

    Update
    Update Mensaje

--- src/interfaces/MessageController.js
+++ src/interfaces/MessageController.js
@@ -2,7 +2,7 @@ import Message from "../../domain/entities/Message.js";
 
 class MessageRepository {
   getMessage() {
-    return new Message("Hola mundo desde Clean Architecture");
+    return new Message("Hola profe");
   }
 }
 
